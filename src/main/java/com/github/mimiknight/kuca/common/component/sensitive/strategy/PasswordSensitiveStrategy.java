051e388a3a634a080350a323fa56d5ff5e34574b
Jack <victor2015yhm@gmail.com>
2023-07-19 23:44:44 +0800

    【关联单号】US202307050831 【提交描述】修改包名前缀为com.github.mimiknight

---
+++ src/main/java/com/github/mimiknight/kuca/common/component/sensitive/strategy/PasswordSensitiveStrategy.java
@@ -0,0 +1,14 @@
+package com.github.mimiknight.kuca.common.component.sensitive.strategy;
+
+/**
+ * 密码敏感战略
+ *
+ * @author victor2015yhm@gmail.com
+ * @since 2023-05-22 01:59:06
+ */
+public class PasswordSensitiveStrategy implements SensitiveStrategy {
+    @Override
+    public String desensitize(String text) {
+        return null;
+    }
+}
---
+++ src/main/java/com/github/mimiknight/kuca/common/exception/RedisException.java
@@ -0,0 +1,29 @@
+package com.github.mimiknight.kuca.common.exception;
+
+/**
+ * Redis故障异常
+ *
+ * @author victor2015yhm@gmail.com
+ * @since 2023-06-03 11:32:04
+ */
+public class RedisException extends RuntimeException {
+
+    public RedisException() {
+        super();
+    }
+
+
+    public RedisException(String s) {
+        super(s);
+    }
+
+
+    public RedisException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public RedisException(Throwable cause) {
+        super(cause);
+    }
+}
